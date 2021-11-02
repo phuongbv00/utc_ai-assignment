5cb862af08039e2b2a8c48ac8de22859b846f327
Phuong Bui <buiphuongaptutc@gmail.com>
2021-11-02 15:19:06 +0700

    resolution prove

--- src/main/java/algorithms/ResolutionProve.java
+++ src/main/java/algorithms/ResolutionProve.java
@@ -152,4 +152,27 @@ public class ResolutionProve {
         @Builder.Default
         private List<String> THOA = new ArrayList<>();
     }
+
+    public static void main(String[] args) {
+        var algorithm = new ResolutionProve();
+        var clauses = List.of(
+                "!PvQ",
+                "PvR",
+                "!Q",
+                "!RvS"
+        );
+        algorithm.propositionProve("S", clauses);
+        algorithm.print();
+
+        clauses = List.of(
+                "!EvF",
+                "!Av!BvC",
+                "!BvD",
+                "!Cv!DvE",
+                "A",
+                "B"
+        );
+        algorithm.propositionProve("F", clauses);
+        algorithm.print();
+    }
 }
